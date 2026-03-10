9984ccf6290c0dba765b787e8ba37ae055ea24bd
zhizhu <zhizhu@zhizhus-MacBook-Pro.local>
2026-03-10 22:04:55 +0700

    feat: upload file to r2

--- src/modules/upload/service/upload.service.ts
+++ src/modules/upload/service/upload.service.ts
@@ -1,24 +1,29 @@
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { urlJoin } from "@common/utils/url-join";
+import { ENV } from "@config/env.config";
 import { Injectable } from "@nestjs/common";
 import { randomUUID } from "crypto";
-
 @Injectable()
 export class UploadService {
   private s3 = new S3Client({
     region: "auto",
-    endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+    endpoint: `https://${ENV.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
     credentials: {
-      accessKeyId: process.env.R2_ACCESS_KEY_ID!,
-      secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!,
+      accessKeyId: ENV.R2_ACCESS_KEY_ID,
+      secretAccessKey: ENV.R2_SECRET_ACCESS_KEY,
     },
+    forcePathStyle: true,
   });
 
-  async upload(file: Express.Multer.File) {
-    const key = `${randomUUID()}-${file.originalname}`;
+  async upload(file: Express.Multer.File, path: string = "test") {
+    const key = urlJoin([path, `${randomUUID()}-${file.originalname}`], {
+      removeLeadingSlash: true,
+      removeTrailingSlash: true,
+    });
 
     await this.s3.send(
       new PutObjectCommand({
-        Bucket: process.env.R2_BUCKET,
+        Bucket: ENV.R2_BUCKET,
         Key: key,
         Body: file.buffer,
         ContentType: file.mimetype,
@@ -27,7 +32,7 @@ export class UploadService {
 
     return {
       key,
-      url: `${process.env.R2_PUBLIC_URL}/${key}`,
+      url: `${ENV.R2_PUBLIC_URL}/${key}`,
     };
   }
 }
