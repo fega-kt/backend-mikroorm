2da9492e4ed6ee7faf3626921a531dbab6e2c5a3
zhizhu <fega.kt@gmail.com>
2026-06-24 22:16:29 +0700

    chore(build): remove build-info script and unused JWT_SECRET

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -13,8 +13,6 @@ export const ENV = {
   CORS_ORIGINS: process.env.CORS_ORIGINS,
   DATABASE_URL: process.env.DATABASE_URL,
   DB_NAME: process.env.DB_NAME || "zhizhu",
-  JWT_SECRET: process.env.JWT_SECRET || "secret",
-
   R2_ACCOUNT_ID: process.env.R2_ACCOUNT_ID,
   R2_ACCESS_KEY_ID: process.env.R2_ACCESS_KEY,
   R2_SECRET_ACCESS_KEY: process.env.R2_SECRET_KEY,
