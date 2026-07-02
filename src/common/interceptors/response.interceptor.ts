b3dcc315c46218cb7580318dc624724aee259a25
zhizhu <fega.kt@gmail.com>
2026-07-02 20:46:53 +0700

    feat(auth): cache auth result per token with 5m ttl to reduce latency
    feat(interceptor): add response time in ms to all api responses

--- src/common/interceptors/response.interceptor.ts
+++ src/common/interceptors/response.interceptor.ts
@@ -2,11 +2,13 @@ import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nes
 import { map, Observable } from "rxjs";
 
 @Injectable()
-export class ResponseInterceptor<T> implements NestInterceptor<T, { result: T }> {
-  intercept(context: ExecutionContext, next: CallHandler): Observable<{ result: T }> {
+export class ResponseInterceptor<T> implements NestInterceptor<T, { result: T; time: number }> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<{ result: T; time: number }> {
+    const start = Date.now();
     return next.handle().pipe(
       map((data) => ({
         result: data,
+        time: Date.now() - start,
       })),
     );
   }
